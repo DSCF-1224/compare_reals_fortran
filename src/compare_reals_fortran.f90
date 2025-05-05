5d049eec7b98b5a8eb0d599e1a9b2374b1bd0abb
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-05-06 00:07:06 +0900

    Add a `module` file

---
+++ src/compare_reals_fortran.f90
@@ -0,0 +1,10 @@
+module compare_reals_fortran
+
+    use, intrinsic :: iso_fortran_env, only: real32
+    use, intrinsic :: iso_fortran_env, only: real64
+    use, intrinsic :: iso_fortran_env, only: real128
+
+
+    implicit none
+
+end module compare_reals_fortran
---
+++ src/run_fypp.sh
@@ -0,0 +1,2 @@
+#!/bin/sh -e
+fypp -F compare_reals_fortran.fypp compare_reals_fortran.f90
