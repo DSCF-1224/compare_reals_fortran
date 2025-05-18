ad9466ca304d38bed8f2576bca1ba796521c89d4
DSCF-1224 <37315588+DSCF-1224@users.noreply.github.com>
2025-05-18 21:00:28 +0900

    Fix workflow file - Fix manifest file - Fix bash file to run test using fpm

--- run_test.sh
+++ run_test.sh
@@ -1,29 +1,12 @@
 #!/bin/sh -e
-cd test
-fypp -F                      compare_reals_fortran_support.fypp compare_reals_fortran_support.f90
-fypp -F -DTARGET_INTERFACE=0 test_common.fypp                   test_intrinsic.f90
-
-cd ..
+fypp -F                      test/compare_reals_fortran_support.fypp test/compare_reals_fortran_support.f90
+fypp -F -DTARGET_INTERFACE=0 test/test_common.fypp                   test/test_intrinsic.f90
 rm -rf build
 fpm test --flag "-Wall -Werror -pedantic -std=f2008 -ffree-line-length-none"
 
-cd test
-rm test_intrinsic.f90
-fypp -F -DTARGET_INTERFACE=1 test_common.fypp eq_transfer.f90
-cd ..
-rm -rf build
-fpm test --flag "-Wall -Wextra -Werror -pedantic -std=f2008 -ffree-line-length-none"
-
-cd test
-rm eq_transfer.f90
-fypp -F -DTARGET_INTERFACE=2 test_common.fypp test_is_contained_by_next_out.f90
-cd ..
-rm -rf build
-fpm test --flag "-Wall -Wextra -Werror -pedantic -std=f2008 -ffree-line-length-none"
-
-cd test
-rm test_is_contained_by_next_out.f90
-fypp -F -DTARGET_INTERFACE=3 test_common.fypp test_le_and_ge.f90
-cd ..
+rm test/test_intrinsic.f90
+fypp -F -DTARGET_INTERFACE=1 test/test_common.fypp test/test_eq_transfer.f90
+fypp -F -DTARGET_INTERFACE=2 test/test_common.fypp test/test_is_contained_by_next_out.f90
+fypp -F -DTARGET_INTERFACE=3 test/test_common.fypp test/test_le_and_ge.f90
 rm -rf build
 fpm test --flag "-Wall -Wextra -Werror -pedantic -std=f2008 -ffree-line-length-none"
